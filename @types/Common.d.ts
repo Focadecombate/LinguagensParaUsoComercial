cdb0ad3525f1c41c3949d7e11ef00081bfb28684
Gustavo Santos <gustavosisa@outlook.com>
2022-11-13 14:46:28 -0300

    feat: add types

---
+++ @types/Common.d.ts
@@ -0,0 +1,20 @@
+export type Store = "DROGASIL" | "DROGA_RAIA" | "DROGARIA_SAO_PAULO";
+
+export interface Result {
+  name: string;
+  price: number;
+  discountedPrice?: number;
+  linkToProduct: string;
+  store: Store;
+  image?: string;
+}
+
+export interface Input {
+  productName: string;
+  longitude: string
+  latitude: string
+}
+
+export interface SearchProducts {
+  search(input: Input): Promise<Result[]>;
+}
