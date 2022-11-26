e97748a033b5aa67ad971883c93c8ab39d82077a
Matheus Carvalho do Carmo <matheuscarvalho@windowslive.com>
2022-11-26 20:16:05 -0300

    add tests

---
+++ api/__tests__/drogasil.test.ts
@@ -0,0 +1,44 @@
+import { expect, test, describe } from '@jest/globals';
+
+import { Drogasil } from '../farmacias';
+
+describe('when DrogaRaia search function is called', () => {
+    const drogasil = new Drogasil();
+    const inputMock = {
+        productName: 'a',
+        longitude: 'b',
+        latitude: 'c'
+    }
+
+    test('should return a array of objects', async () => {
+
+        try {
+
+            const result = await drogasil.search(inputMock);
+
+            expect(Array.isArray(result)).toBe(true);
+            expect(typeof result[0]).toBe('object');
+        } catch (_) {
+
+        }
+
+    })
+
+    test('should return attributes types correctly', async () => {
+
+        try {
+
+            const result = await drogasil.search(inputMock);
+
+            expect(typeof result[0].name).toBe('string');
+            expect(typeof result[0].image).toBe('string');
+            expect(typeof result[0].linkToProduct).toBe('string');
+            expect(typeof result[0].price).toBe('number');
+            expect(typeof result[0].discountedPrice).toBe('number');
+            expect(typeof result[0].store).toBe('string');
+        } catch (_) {
+
+        }
+
+    })
+})
