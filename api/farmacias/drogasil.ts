3fb3a36ae286103919262a8bbada32737a009f80
emanuelvs <emanuelvisantos99@gmail.com>
2022-12-05 23:45:42 -0300

    adding store locations to drogasil

--- api/farmacias/drogasil.ts
+++ api/farmacias/drogasil.ts
@@ -2,6 +2,7 @@ import axios, { AxiosInstance } from "axios";
 import { Input, Result, SearchProducts } from "../@types/Common";
 import { DrogasilAndDrogaRaiaResponse } from "../@types/Drogasil";
 import { pharmacyUrls } from "../constants";
+import { GeolocationUtil } from "../helpers/geolocationUtil";
 
 export class Drogasil implements SearchProducts {
   private readonly axiosInstance: AxiosInstance;
@@ -25,6 +26,9 @@ export class Drogasil implements SearchProducts {
     );
 
     const products = data.results.products;
+
+    const store_locations = await this.getStoreLocation(input.latitude, input.longitude);
+
     const results: Result[] = products.map((product: any) => ({
       name: product.name,
       price: product.valueTo,
@@ -32,8 +36,36 @@ export class Drogasil implements SearchProducts {
       store: "DROGASIL",
       image: product.image,
       discountedPrice: product.valueFrom,
+      store_locations
     }));
 
     return results;
+  }
+
+  async getStoreLocation(lat: string, lon: string) {
+    let geo = new GeolocationUtil();
+    const data = await geo.getAddress(lat, lon);
+    
+    const stores = await axios.post("https://site-bff-prod.drogasil.com.br/graphql", {
+      
+        operationName: "getStores",
+        variables: {
+            stores: data?.address?.suburb,
+            regionName: "",
+            cityName: data?.address?.city,
+            neighborhood: "",
+            fullTime: "",
+            parking: "",
+            pharmacyPopular: "",
+            psychotropic: "",
+            itensPerPage: 24,
+            activePage: 1
+        },
+        query: "query getStores($stores: String!, $regionName: String!, $cityName: String!, $neighborhood: String!, $fullTime: String!, $parking: String!, $pharmacyPopular: String!, $psychotropic: String!, $itensPerPage: Int!, $activePage: Int!) {\n  stores(\n    input: {searchText: {match: $stores}, regionName: {match: $regionName}, cityName: {match: $cityName}, neighborhood: {match: $neighborhood}, fullTime24h: {eq: $fullTime}, parking: {eq: $parking}, pharmacyPopular: {eq: $pharmacyPopular}, psychotropic: {eq: $psychotropic}, sort: {id: ASC}, pageSize: $itensPerPage, currentPage: $activePage}\n  ) {\n    page_info {\n      current_page\n      page_size\n      total_pages\n      total_count\n      __typename\n    }\n    items {\n      fantasyName\n      telephone\n      telephoneAreaCode\n      pharmacyPopular\n      psychotropic\n      parking\n      fullTime24h\n      address {\n        regionName\n        regionId\n        cityId\n        cityName\n        neighborhood\n        street\n        number\n        postcode\n        __typename\n      }\n      labelAttendance\n      __typename\n    }\n    filters {\n      regionName\n      cityName\n      neighborhood\n      pharmacyPopular\n      parking\n      fullTime24h\n      psychotropic\n      __typename\n    }\n    __typename\n  }\n}\n"
+      
+    });
+    
+    return stores.data?.data?.stores?.items;
+    
   }
 }
