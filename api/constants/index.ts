91f03d4b378236b700af1eb01c19d74cbce9bbc0
emanuelvs <emanuelvisantos99@gmail.com>
2022-12-12 23:05:04 -0300

    adding drogariaSp stores locations

--- api/constants/index.ts
+++ api/constants/index.ts
@@ -6,6 +6,12 @@ export const pharmacyUrls: Record<Store, string> = {
     DROGARIA_SAO_PAULO: "https://api.linximpulse.com/engage/search/v3",
 };
 
+export const pharmacyLocationUrls = {
+  DROGA_RAIA: "https://bff.drogaraia.com.br/graphql",
+  DROGASIL: "https://site-bff-prod.drogasil.com.br/graphql",
+  DROGARIA_SAO_PAULO: "https://www.drogariasaopaulo.com.br/api/dataentities/PR/documents/f52e9e7f-a02c-11ea-8337-0a8ac637298d/arquivo/attachments/nossas-lojas.js",
+}
+
 export const helpers = {
   NOMINATIM: "https://nominatim.openstreetmap.org"
 }
